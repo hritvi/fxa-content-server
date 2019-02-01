7f54251fed9f38f1c14c6c0192ffc4208dbab130
Phil Booth <pmbooth@gmail.com>
2019-02-01 13:06:02 +0000

    feat(feature-flags): wire in experiments to the feature-flag api

--- server/lib/routes/get-index.js
+++ server/lib/routes/get-index.js
@@ -8,6 +8,8 @@ const flowMetrics = require('../flow-metrics');
 const logger = require('../logging/log')('routes.index');
 
 module.exports = function (config) {
+  const featureFlags = require('fxa-shared/feature-flags')(config.get('featureFlags'), {});
+
   const AUTH_SERVER_URL = config.get('fxaccount_url');
   const CLIENT_ID = config.get('oauth_client_id');
   const COPPA_ENABLED = config.get('coppa.enabled');
@@ -49,7 +51,7 @@ module.exports = function (config) {
   return {
     method: 'get',
     path: '/',
-    process: function (req, res) {
+    process: async function (req, res) {
       const flowEventData = flowMetrics.create(FLOW_ID_KEY, req.headers['user-agent']);
 
       if (NO_LONGER_SUPPORTED_CONTEXTS.has(req.query.context)) {
@@ -60,6 +62,7 @@ module.exports = function (config) {
         // Note that bundlePath is added to templates as a build step
         bundlePath: '/bundle',
         config: serializedConfig,
+        featureFlags: encodeURIComponent(JSON.stringify(await featureFlags.get())),
         flowBeginTime: flowEventData.flowBeginTime,
         flowId: flowEventData.flowId,
         // Note that staticResourceUrl is added to templates as a build step
